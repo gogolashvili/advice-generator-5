c5516fc87d2879880366a55cd4a75c47f7fce204
gogolashvili <ggogolashvili91@gmail.com>
2023-02-28 13:24:43 +0400

    add function

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let number = document.querySelector(".number");
 let result = document.querySelector(".result");
 let button = document.querySelector(".butimg");
 
+window.addEventListener("load", function () {
+  axios.get("https://api.adviceslip.com/advice").then(function (name) {
+    console.log(name.data.slip);
+    number.innerHTML = name.data.slip.id;
+    result.innerHTML = name.data.slip.advice;
+  });
+});
+
 button.addEventListener("click", () => {
   axios.get("https://api.adviceslip.com/advice").then(function (name) {
     console.log(name.data.slip);
