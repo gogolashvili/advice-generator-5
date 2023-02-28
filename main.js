d6254430be2ee91aeb801c95a25ef9bd247ba007
gogolashvili <ggogolashvili91@gmail.com>
2023-02-28 12:56:48 +0400

    add function

---
+++ main.js
@@ -0,0 +1,11 @@
+let number = document.querySelector(".number");
+let result = document.querySelector(".result");
+let button = document.querySelector(".butimg");
+
+button.addEventListener("click", () => {
+  axios.get("https://api.adviceslip.com/advice").then(function (name) {
+    console.log(name.data.slip);
+    number.innerHTML = name.data.slip.id;
+    result.innerHTML = name.data.slip.advice;
+  });
+});
